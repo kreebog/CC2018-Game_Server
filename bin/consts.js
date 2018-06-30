d2d5d9f9b1f1076104389553f7ffc938b5247149
James Dean <kreebog@gmail.com>
2018-06-29 22:33:37 -0400

    working

--- bin/consts.js
+++ bin/consts.js
@@ -6,9 +6,9 @@ const util_1 = require("util");
 exports.NODE_ENV = process.env.NODE_ENV || 'PROD';
 // load env vars (or defaults if not found - aka PROD)
 exports.MAZE_SVC_HOST = process.env.MAZE_SVC_URL || 'http://maze-service-code-camp.a3c1.starter-us-west-1.openshiftapps.com';
-exports.MAZE_SVC_PORT = process.env.MAZE_SVC_PORT || 80;
+exports.MAZE_SVC_PORT = process.env.MAZE_SVC_PORT || 8080;
 exports.SCORE_SVC_HOST = process.env.SCORE_SVC_URL || 'http://score-service-code-camp.a3c1.starter-us-west-1.openshiftapps.com';
-exports.SCORE_SVC_PORT = process.env.SCORE_SVC_PORT || 80;
+exports.SCORE_SVC_PORT = process.env.SCORE_SVC_PORT || 8080;
 exports.GAME_SVC_PORT = process.env.GAME_SVC_PORT || 8080;
 // construct base URLs
 exports.MAZE_SVC_URL = util_1.format('%s:%s', exports.MAZE_SVC_HOST, exports.MAZE_SVC_PORT);
--- src/consts.ts
+++ src/consts.ts
@@ -6,9 +6,9 @@ export const NODE_ENV = process.env.NODE_ENV || 'PROD';
 
 // load env vars (or defaults if not found - aka PROD)
 export const MAZE_SVC_HOST = process.env.MAZE_SVC_URL || 'http://maze-service-code-camp.a3c1.starter-us-west-1.openshiftapps.com';
-export const MAZE_SVC_PORT = process.env.MAZE_SVC_PORT || 80;
+export const MAZE_SVC_PORT = process.env.MAZE_SVC_PORT || 8080;
 export const SCORE_SVC_HOST = process.env.SCORE_SVC_URL || 'http://score-service-code-camp.a3c1.starter-us-west-1.openshiftapps.com';
-export const SCORE_SVC_PORT = process.env.SCORE_SVC_PORT || 80;
+export const SCORE_SVC_PORT = process.env.SCORE_SVC_PORT || 8080;
 export const GAME_SVC_PORT = process.env.GAME_SVC_PORT || 8080;
 
 // construct base URLs
